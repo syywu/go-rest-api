fed7df57fc6f73403b06da0061f9728bd43889ad
Samantha Wu <samanthayywu@gmail.com>
2023-02-20 16:20:59 +0000

    homeHandler

--- main.go
+++ main.go
@@ -8,12 +8,21 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
+// posts
+/*
+{
+    "userId": 1,
+    "id": 1,
+    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+  },*/
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World!"))
+}
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World!"))
-	})
+	r.Get("/", homeHandler)
 	fmt.Print("listening on port 8080")
 	http.ListenAndServe(":8080", r)
 }
