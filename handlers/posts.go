158a09c2b31329c639c3e34702141b85ca753a9e
Samantha Wu <samanthayywu@gmail.com>
2023-02-22 16:43:36 +0000

    200 status code

--- handlers/posts.go
+++ handlers/posts.go
@@ -6,8 +6,9 @@ import (
 	"net/http"
 )
 
-func GetPosts(allPosts []models.Post) http.HandlerFunc {
+func GetPosts(allPosts *models.List) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode(allPosts)
+		w.WriteHeader(200)
 	}
 }
