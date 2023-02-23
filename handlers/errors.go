c5471907712ac401fbde77bac39c21e569eda2fb
Samantha Wu <samanthayywu@gmail.com>
2023-02-23 22:22:10 +0000

    render func

--- handlers/errors.go
+++ handlers/errors.go
@@ -1,5 +1,11 @@
 package handlers
 
+import (
+	"net/http"
+
+	"github.com/go-chi/render"
+)
+
 type ErrResponse struct {
 	Err        error  `json:"-"`
 	StatusCode int    `json:"-"`
@@ -12,3 +18,8 @@ var (
 	ErrNotFound         = &ErrResponse{StatusCode: 404, Message: "Not found"}
 	ErrBadRequest       = &ErrResponse{StatusCode: 400, Message: "Bad request"}
 )
+
+func (e *ErrResponse) Render(w http.ResponseWriter, r *http.Request) error {
+	render.Status(r, e.StatusCode)
+	return nil
+}
