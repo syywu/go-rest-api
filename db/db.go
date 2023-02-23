29a9585dca89e22eb8a8da53f3a1a88309818453
Samantha Wu <samanthayywu@gmail.com>
2023-02-23 15:45:41 +0000

    initalise db

--- db/db.go
+++ db/db.go
@@ -3,6 +3,7 @@ package db
 import (
 	"database/sql"
 	"fmt"
+	"log"
 )
 
 const (
@@ -14,4 +15,20 @@ var ErrNoMatch = fmt.Errorf("no matching record")
 
 type Db struct {
 	Conn *sql.DB
+}
+
+func Initialise(username, password, database string) (Db, error) {
+	db := Db{}
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", HOST, PORT, username, password, database)
+	conn, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return db, err
+	}
+	db.Conn = conn
+	err = db.Conn.Ping()
+	if err != nil {
+		return db, err
+	}
+	log.Println("Database connection established")
+	return db, nil
 }
