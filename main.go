8fbc070637886cbfe6d08330bed39a8826882219
Samantha Wu <samanthayywu@gmail.com>
2023-02-24 10:18:48 +0000

    db connection main

--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
-	"myapi/handlers"
-	"myapi/models"
+	"myapi/db"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -14,9 +13,9 @@ import (
 )
 
 func main() {
-	var allPosts = []*models.Post{
-		{UserId: 100, Id: 1, Title: "hello", Body: "hello world"},
-	}
+	// var allPosts = []*models.Post{
+	// 	{UserId: 100, Id: 1, Title: "hello", Body: "hello world"},
+	// }
 	// allPosts := models.New()
 
 	// allPosts.Add(models.Post{
@@ -38,34 +37,43 @@ func main() {
 		w.Write([]byte("Root Route"))
 	})
 
-	r.Route("/posts", func(r chi.Router) {
-		r.Get("/", handlers.GetPosts(allPosts))
-		// post
-		r.Post("/", func(w http.ResponseWriter, r *http.Request) {
-			req := &models.Post{}
-			// allPosts = append(allPosts, req)
-			// allPosts.Add(models.Post{
-			// 	UserId: req["userid"],
-			// 	Id:     req["id"],
-			// 	Title:  req["title"],
-			// 	Body:   req["body"],
-			// })
-			// req.Id = rand.Intn(100) + 10
-			allPosts = append(allPosts, req)
-			json.NewDecoder(r.Body).Decode(req)
-			w.Write([]byte("posted"))
-			render.Status(r, http.StatusCreated)
+	/*
+		r.Route("/posts", func(r chi.Router) {
+			r.Get("/", handlers.GetPosts(allPosts))
+			// post
+			r.Post("/", func(w http.ResponseWriter, r *http.Request) {
+				req := &models.Post{}
+				// allPosts = append(allPosts, req)
+				// allPosts.Add(models.Post{
+				// 	UserId: req["userid"],
+				// 	Id:     req["id"],
+				// 	Title:  req["title"],
+				// 	Body:   req["body"],
+				// })
+				// req.Id = rand.Intn(100) + 10
+				allPosts = append(allPosts, req)
+				json.NewDecoder(r.Body).Decode(req)
+				w.Write([]byte("posted"))
+				render.Status(r, http.StatusCreated)
+			})
 		})
-	})
 
-	r.Route("/{postID}", func(r chi.Router) {
-		// delete
-		r.Delete("/", handlers.DeletePost())
+		r.Route("/{postID}", func(r chi.Router) {
+			// delete
+			r.Delete("/", handlers.DeletePost())
 
-		// patch
+			// patch
 
-		// put
-	})
+			// put
+		})
+	*/
+	dbUser, dbPassword, dbName := os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB")
+
+	database, err := db.Initialise(dbUser, dbPassword, dbName)
+	if err != nil {
+		log.Fatal("cannot connect to db", err)
+	}
+	defer database.Conn.Close()
 
 	fmt.Print("listening on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
