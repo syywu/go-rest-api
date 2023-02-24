7562069cddbe6929d92efdad3fb8f02f6b4975cf
Samantha Wu <samanthayywu@gmail.com>
2023-02-24 10:29:13 +0000

    fixed typos in errors.go

--- handlers/errors.go
+++ handlers/errors.go
@@ -9,8 +9,8 @@ import (
 type ErrResponse struct {
 	Err        error  `json:"-"`
 	StatusCode int    `json:"-"`
-	StatusText string `json:"status_text`
-	Message    string `json:"message`
+	StatusText string `json:"status_text"`
+	Message    string `json:"message"`
 }
 
 var (
