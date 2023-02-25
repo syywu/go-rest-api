176b286b9003b9b53b1036f0e562dcfbef4b87a5
Samantha Wu <samanthayywu@gmail.com>
2023-02-25 14:40:05 +0000

    root and port

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
 func main() {
 
+	r := chi.NewRouter()
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("root route"))
+	})
+
+	fmt.Print("Listening on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
