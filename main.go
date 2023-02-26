b6301455abf1dd4eabb984bb4bd443bf94d37eea
Samantha Wu <samanthayywu@gmail.com>
2023-02-26 22:15:38 +0000

    db fully interacting with api- GET and POST

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ import (
 
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/render"
 )
 
 type Post struct {
@@ -21,21 +20,24 @@ type Post struct {
 	Body   string `json:"body"`
 }
 
-var db *sql.DB
-
-func main() {
+func OpenConnection() *sql.DB {
 	db, err := sql.Open("postgres", "postgres://user:password@localhost/data?sslmode=disable")
 	if err != nil {
 		log.Fatal("Cannot connect to db", err)
 	} else {
 		log.Println("Database connection established")
 	}
-	defer db.Close()
 
 	err = db.Ping()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	return db
+}
+
+func CreateTable() {
+	db := OpenConnection()
 
 	var createPostTable = `
 	CREATE TABLE IF NOT EXISTS posts(
@@ -45,39 +47,40 @@ func main() {
 	body TEXT
 	);
 	`
-
-	_, err = db.Exec(createPostTable)
+	_, err := db.Exec(createPostTable)
 	if err != nil {
 		log.Fatal("cannot create table", err)
 	}
+	defer db.Close()
+}
+
+func main() {
 
 	r := chi.NewRouter()
-
-	r.Use(middleware.RequestID)
+	CreateTable()
+	// r.Use(middleware.RequestID)
 	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
-	r.Use(render.SetContentType(render.ContentTypeJSON))
+	// r.Use(middleware.Recoverer)
+	// r.Use(render.SetContentType(render.ContentTypeJSON))
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("root route"))
 	})
 
-	r.Route("/posts", func(r chi.Router) {
-		r.Get("/", GetAllPosts)
-		r.Post("/", AddPost)
-	})
+	r.Get("/posts", GetAllPosts)
+	r.Post("/posts", AddPost)
 
 	fmt.Print("Listening on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
 func GetAllPosts(w http.ResponseWriter, r *http.Request) {
+	db := OpenConnection()
 	rows, err := db.Query("SELECT * FROM posts")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	defer rows.Close()
 
 	posts := []Post{}
 
@@ -91,13 +94,14 @@ func GetAllPosts(w http.ResponseWriter, r *http.Request) {
 		posts = append(posts, post)
 	}
 	w.Header().Set("Content-Type", "application/json")
-	err = json.NewEncoder(w).Encode(posts)
-	if err != nil {
-		log.Fatal(err)
-	}
+	json.NewEncoder(w).Encode(posts)
+
+	defer rows.Close()
+	defer db.Close()
 }
 
 func AddPost(w http.ResponseWriter, r *http.Request) {
+	db := OpenConnection()
 	var post Post
 	err := json.NewDecoder(r.Body).Decode(&post)
 	if err != nil {
@@ -105,10 +109,10 @@ func AddPost(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	res, err := db.Exec("INSERT INTO posts (userid, title, body) VALUES ($1, $2, $3) RETURNING id", post.UserId, post.Title, post.Body)
+	_, err = db.Exec("INSERT INTO posts (userid, title, body) VALUES ($1, $2, $3)", post.UserId, post.Title, post.Body)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		log.Fatal(err)
 	}
-
+	w.WriteHeader(http.StatusCreated)
+	defer db.Close()
 }
