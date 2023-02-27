55cadb24b83ed305fbad8569379dfc85673199f5
Samantha Wu <samanthayywu@gmail.com>
2023-02-27 16:01:43 +0000

    cors

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.19
 require (
 	github.com/go-chi/chi v1.5.4
 	github.com/go-chi/chi/v5 v5.0.8
+	github.com/go-chi/cors v1.2.1
 	github.com/lib/pq v1.10.7
 )
--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 )
 
 func main() {
@@ -21,6 +22,18 @@ func main() {
 	// r.Use(middleware.URLFormat)
 	// r.Use(middleware.Recoverer)
 	// r.Use(render.SetContentType(render.ContentTypeJSON))
+
+	// Basic CORS
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 
 	db.CreateTable()
 
